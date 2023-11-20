2c186c45d8c939a3791357e8a16a49c31f8719fd
Sirma-cloud <kevinkiplimo99@gmail.com>
2023-11-20 22:39:32 +0300

    Uploading csv using springboot

---
+++ src/main/java/com/sirma/springbootfileupload/Student/StudentCsvRepresentation.java
@@ -0,0 +1,20 @@
+package com.sirma.springbootfileupload.Student;
+
+import com.opencsv.bean.CsvBindByName;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class StudentCsvRepresentation {
+
+    @CsvBindByName (column =  "firstname")
+    private String fname;
+    @CsvBindByName (column =  "lastname")
+    private String lname;
+    @CsvBindByName (column =  "age")
+    private int age;
+
+}
